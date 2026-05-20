416fe5e33ca14d7594c49c303453f37e50c30ee5
Vishal-bhurke <shubhambhurke24@gmail.com>
2026-05-20 13:27:04 +0530

    fix(testimonials): update quotes for clarity and consistency

--- src/constants/testimonials.js
+++ src/constants/testimonials.js
@@ -1,7 +1,7 @@
 export const testimonials = [
   {
     id: 1,
-    quote: 'Folk Aakhyan’s show felt nothing short of a miracle. All the children performed with incredible energy and enthusiasm, lifting the entire atmosphere and holding everyone’s attention beautifully.',
+    quote: 'It truly felt like a miracle. All the children performed with such energy and enthusiasm, lifting the entire atmosphere and holding everyone’s attention beautifully',
     name: 'Amruta Subhash',
     role: 'Actress',
     rating: 5,
@@ -10,7 +10,7 @@ export const testimonials = [
   },
   {
     id: 2,
-    quote: 'Folk Aakhyan’s musical journey was engaging and beautifully presented. Please continue organizing such meaningful programs and keep giving us opportunities to experience such performances.',
+    quote: 'The musical journey was wonderful and engaging throughout. Please continue organizing such programs and keep giving us opportunities to witness and enjoy these performances.',
     name: 'Sumeet Pusawale',
     role: 'Actor',
     rating: 5,
@@ -20,15 +20,15 @@ export const testimonials = [
   {
     id: 3,
     quote:
-      'After the show, even though everything became calm, the impact stayed with everyone. Some participants had disabilities, yet they joined wholeheartedly and enjoyed every moment with great spirit. sWatching them dance with music was truly overwhelming for us. Our shows usually last around 2.5 to 3 hours, but during this performance, we didn’t even realize how time passed — it extended to 3.5 to 4 hours because of the incredible energy and involvement',
+      'After the program ended, everyone was calm, but the impact remained strong. Some participants had disabilities, yet they joined in wholeheartedly and enjoyed every moment with great spirit.Watching them dance with the music was truly overwhelming for us. Our shows usually last around 2.5 to 3 hours, but here we didn’t even realize how time passed — it extended to 3.5 or even 4 hours because of the energy and involvement.',
     name: 'Team – Folk Aakan!',
     role: '',
     rating: 5,
     image:'folk-akhyan.jpg',
   },
-    {
+  {
     id: 4,
-    quote: 'It was truly heartwarming to witness Folk Aakhyan’s show. Seeing all the children participate with such joy and excitement, coming together as one, brought immense happiness.',
+    quote: 'It was heartwarming to see all the children participating with such joy and excitement. Watching everyone come together as one and celebrate the moment brought immense happiness',
     name: 'Sanadesh Kulkarni',
     role: 'Actor',
     rating: 5,
